30fd869fc5abb2151ee0ca29cdef2678b74d58d2
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2022-11-18 18:02:29 -0500

    first commit

---
+++ app/src/main/java/zabdi011/uottawa/lab8/MainActivity.java
@@ -0,0 +1,14 @@
+package zabdi011.uottawa.lab8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
